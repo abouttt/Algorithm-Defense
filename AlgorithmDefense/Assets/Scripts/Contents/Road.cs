b414b34b232b51b46fe7b5136a70df80167fa325
abouttt <qw4898@naver.com>
2022-05-29 19:30:44 +0900

    Delete all

--- AlgorithmDefense/Assets/Scripts/Contents/Road.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Road : MonoBehaviour
-{
-    [field: SerializeField]
-    public Define.RoadType RoadType { get; private set; }
-}
