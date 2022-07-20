e57119e1ed7ae7900f61af6f625efc2c190e64ae
abouttt <qw4898@naver.com>
2022-07-20 15:11:00 +0900

    Remove All

--- AlgorithmDefense/Assets/Scripts/TileObject/Road.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Road : MonoBehaviour
-{
-    [field: SerializeField]
-    public Define.Road RoadType { get; private set; }
-}
--- AlgorithmDefense/Assets/Scripts/UI/Scene/UI_CitizenSpawnButton.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class UI_CitizenSpawnButton : MonoBehaviour
-{
-    [field: SerializeField]
-    public Define.Citizen CitizenType { get; private set; }
-}
