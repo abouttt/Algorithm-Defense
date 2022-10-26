31ed9c621668b216955c131c01b2ff3172fc6325
Hero-K98 <hero9812@gmail.com>
2022-10-26 19:48:53 +0900

    오더 수정

--- AlgorithmDefense/Assets/Scripts/Scenes/GameScene.cs
+++ AlgorithmDefense/Assets/Scripts/Scenes/GameScene.cs
@@ -45,7 +45,7 @@ public class GameScene : MonoBehaviour
         InitRampart();
         InitSpawn();
         InitBattleLine();
-
+        Managers.Game.Gold = 0;
         Managers.Pool.Init();
         Managers.Game.CastleHP = (int)Managers.Game.Setting.CastleMaxHP;
         Managers.Game.DungeonHP = (int)Managers.Game.Setting.DungeonMaxHP;
