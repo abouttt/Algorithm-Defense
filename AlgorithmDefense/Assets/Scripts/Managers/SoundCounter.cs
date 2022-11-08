ebab985469fd9d3a3325da3c267b0469bf380fbf
abouttt <qw4898@naver.com>
2022-11-08 16:55:26 +0900

    사운드 스킵 추가.

--- AlgorithmDefense/Assets/Scripts/Managers/SoundCounter.cs
+++ AlgorithmDefense/Assets/Scripts/Managers/SoundCounter.cs
@@ -5,17 +5,26 @@ using UnityEngine;
 public class SoundCounter : MonoBehaviour
 {
     private Dictionary<AudioClip, int> _audioClipCounts = new();
+    private Dictionary<AudioClip, bool> _audioPlaying = new();
     private int _maxDuplicateOneShotAudioClips = 5;
+    private float _soundSkipTime = 0.1f;
 
     public bool CanPlayOneShot(AudioClip audioClip)
     {
         if (!_audioClipCounts.ContainsKey(audioClip))
         {
             _audioClipCounts.Add(audioClip, 0);
+            _audioPlaying.Add(audioClip, false);
         }
 
         if (_audioClipCounts[audioClip] >= _maxDuplicateOneShotAudioClips)
         {
+            return false;
+        }
+
+        if (_audioPlaying[audioClip] == true)
+        {
+            Debug.Log("IsPlaying");
             return false;
         }
 
@@ -35,12 +44,26 @@ public class SoundCounter : MonoBehaviour
         }
 
         _audioClipCounts[audioClip]++;
+        _audioPlaying[audioClip] = true;
         StartCoroutine(DecreaseAudioClipCount(audioClip));
+        StartCoroutine(AudioPlayingClear(audioClip));
+    }
+
+    public void Clear()
+    {
+        _audioClipCounts.Clear();
+        _audioPlaying.Clear();
     }
 
     private IEnumerator DecreaseAudioClipCount(AudioClip audioClip)
     {
         yield return new WaitForSeconds(audioClip.length);
         _audioClipCounts[audioClip]--;
+    }
+
+    private IEnumerator AudioPlayingClear(AudioClip audioClip)
+    {
+        yield return new WaitForSeconds(_soundSkipTime);
+        _audioPlaying[audioClip] = false;
     }
 }
